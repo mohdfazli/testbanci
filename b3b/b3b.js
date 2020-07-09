1a19ba1c2c2830dffc41f8f33cad282d7e5fcc88
Mohd Fazli Bin Jafrin <fazli@digiheritage.com.my>
2020-07-09 09:46:17 +0800

    new senarai borang 3B features

--- b3b/b3b.js
+++ b3b/b3b.js
@@ -2,16 +2,21 @@
 
 //set first data in local storage for 3B form if empty in local storage ( this will run for first time only to prevent error)
 if (localStorage.getItem("b2b") === null) {
+  var ts = new Date();
+  var qs = ts.toLocaleString();
   myObj = {
-    "f":[ {mykad:'', tvisit:''} ]
+    "f":[ {mykad:'', tvisit: qs } ]
   };
   myJSON = JSON.stringify(myObj);
   localStorage.setItem("b2b", myJSON);
+
 }
 //add new empty 3B form
 function newform(){
+  var ts = new Date();
+  var qs = ts.toLocaleString();
   myObj = JSON.parse(window.localStorage.getItem('b2b'));
-  p = {mykad: "", tvisit: ""};
+  p = {mykad: "", tvisit: qs };
   myObj.f.push(p);
   myJSON = JSON.stringify(myObj);
   localStorage.setItem("b2b", myJSON);
@@ -21,7 +26,7 @@ myObj = JSON.parse(localStorage.getItem('b2b'));
 //generate list of all 3B form that exist in local storage including empty form
 k=0;
 while(k in myObj.f){
-  $( "#3blist" ).append( "<div onclick='localStorage.fid="+(k+1)+";'><a href=index.html>Borang "+(k+1)+" : "+myObj.f[k].mykad+"</a></div>" );
+  $( "#3blist" ).append( "<div onclick='localStorage.fid="+(k+1)+";'><a href=index.html>Borang"+(k+1)+" Mykad : "+myObj.f[k].mykad+"</a> created on : "+myObj.f[k].tvisit+"</div>" );
   k=k+1;
 }
-$( "#3blist" ).append( "<div onclick='localStorage.fid="+(k+1)+";newform();'><a href=index.html>Isi borang 3B baru "+(k+1)+" :</a></div>" );
+$( "#3blist" ).append( "<div class='mt-4' onclick='localStorage.fid="+(k+1)+";newform();'><a href=index.html>+ Isi borang 3B baru :</a></div>" );
